3de0a0afd5401e6e54efe3ca196be96c4f327fca
nyrank <nxkommu@gmail.com>
2020-03-27 17:08:33 -0500

    initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+  <app-nav> </app-nav>
+
+    <router-view class="container"/>
+  </div>
+</template>
+
+<script>
+import AppNav from '@/components/AppNav.vue';
+
+export default {
+  components: {
+    AppNav,
+  },
+};
+
+</script>
+
+<style lang="scss">
+
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ client/src/components/AppNav.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="#">Community</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse"
+     data-target="#navbarColor01" aria-controls="navbarColor01"
+     aria-expanded="true" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+</nav>
+</template>
