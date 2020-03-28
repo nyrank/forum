1ab31f5c80f90a7428b56f152e208f08d946d392
nyrank <nxkommu@gmail.com>
2020-03-27 19:02:22 -0500

    server files

---
+++ server/middlewares/index.js
@@ -0,0 +1,20 @@
+function notFound(req, res, next) {
+    const error = new Error('Not Found - ' + req.originalUrl);
+    res.status(404);
+    next(error);
+}
+
+function errorHandler(error, req, res, next) {
+    res.status(res.statusCode || 500);
+    res.json({
+        message: error.message,
+        error: process.env.NODE_ENV === 'production' ? {} : errorstack,
+    });
+}
+
+module.exports = {
+
+    notFound,
+    errorHandler,
+}
+
